6f068776dc5fb17bfdc35228395a80728c29a544
omkeshborse <bomkes25@gmail.com>
2023-09-16 10:30:56 +0530

    feat :  modal-componet

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Home from "./Home";
+import Modal from "./Modal";
+import SideBar from "./SideBar";
+
+const App = () => {
+  return < >
+    <Home/>
+    <Modal/>
+    <SideBar/>
+  </>;
+};
+export default App;
---
+++ src/Home.jsx
@@ -0,0 +1,13 @@
+import { useGlobalContext } from "./Context"
+import { FaBars } from 'react-icons/fa'
+const Home = () => {
+ const { openModal, openSideBar } = useGlobalContext(); 
+  
+  return (
+    <main>
+        <button onClick={openSideBar} className="sidebar-toggle"><FaBars/> </button>
+        <button onClick={openModal} className="btn"> show modal</button>
+    </main>
+  )
+}
+export default Home
